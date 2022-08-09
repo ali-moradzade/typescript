2733f7d6777fbfc05f68ff8000c57f6cdc1286c8
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 14:05:22 +0430

    adding 023-inference-around-functions.mp4

--- 02-features/annotations/functions.ts
+++ 02-features/annotations/functions.ts
@@ -1,3 +1,14 @@
 const add = (a: number, b: number): number => {
     return a + b;
 };
+
+// Type inference
+// But we don't use it!
+const subtract = (a: number, b: number) => {
+    return a - b;
+}
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
+}
+
