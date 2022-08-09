24c86538868fa2b05b8acf568837bf54b01f398b
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 12:12:41 +0430

    adding 015-object-literal-annotations.mp4

--- 02-features/annotations/variables.ts
+++ 02-features/annotations/variables.ts
@@ -7,5 +7,23 @@ let nothing: undefined = undefined;
 // built-in objects
 let now: Date = new Date();
 
-apples = 10;
-// apples = 'asdfdafk;l'; // Error!
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: {
+    x: number;
+    y: number;
+} = {
+    x: 10,
+    y: 20,
+}
