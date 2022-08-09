476e21ddf15dd1d0b8be6f78950c9bed3f96f11d
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 12:02:56 +0430

    adding 014-annotations-with-variables.mp4

---
+++ 02-features/annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built-in objects
+let now: Date = new Date();
+
+apples = 10;
+// apples = 'asdfdafk;l'; // Error!
