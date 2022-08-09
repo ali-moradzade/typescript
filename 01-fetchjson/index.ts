3e2023156978e29b9ed158ddff2f0340bd478936
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 10:45:39 +0430

    adding 005-one-quick-change.mp4

--- 01-fetchjson/index.ts
+++ 01-fetchjson/index.ts
@@ -4,5 +4,15 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url)
     .then(response => {
-        console.log(response.data);
+        const todo = response.data;
+
+        const id = todo.id;
+        const title = todo.title;
+        const finished = todo.finished;
+
+        console.log(`
+The Todo with id: ${id}
+Has a title of: ${title}
+Is it finished? ${finished}
+        `);
     });
