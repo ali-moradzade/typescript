535afe00f08636fa8bda5291ef2ecf8da1a4c93d
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 14:41:58 +0430

    adding 04-annotations-with-functions-and-objects

--- 02-features/annotations/functions.ts
+++ 02-features/annotations/functions.ts
@@ -3,12 +3,36 @@ const add = (a: number, b: number): number => {
 };
 
 // Type inference
-// But we don't use it!
 const subtract = (a: number, b: number) => {
     return a - b;
 }
 
-const multiply = (a: number, b: number): number => {
-    return a * b;
+// But we don't use it!
+function divide(a: number, b: number): number {
+    return a / b;
 }
+
+const multiply = function (a: number, b: number): number {
+    return a * b;
+};
+
+const logger = (message: string): void => {
+    console.log(message);
+};
+
+const throwError = (message: string): never => {
+    throw new Error(message);
+}
+
+const forecast = {
+    date: new Date(),
+    weather: 'sunny'
+};
+
+const logWeather = (forecast: { date: Date, weather: string }): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+}
+
+logWeather(forecast)
 
