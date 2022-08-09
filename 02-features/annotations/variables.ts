46767df03158898364c2dc3450266165b9242181
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-09 13:31:34 +0430

    adding 018-the-any-type.mp4

--- 02-features/annotations/variables.ts
+++ 02-features/annotations/variables.ts
@@ -32,3 +32,9 @@ let point: {
 const logNumber: (i: number)=> void = (i: number) => {
     console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
