a6e877b3a6726468b5392052902e8e9793158c6a
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-16 11:41:11 +0430

    adding 14-express-typescript-integration

---
+++ 14-express-typescript-integration/Boat.js
@@ -0,0 +1,9 @@
+var Boat = /** @class */ (function () {
+    function Boat() {
+        this.color = 'red';
+    }
+    Boat.prototype.pilot = function () {
+        console.log('swish');
+    };
+    return Boat;
+}());
