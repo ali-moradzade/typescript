c087da8a0a72c0e9354db29fd2c40c7682adba86
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-16 10:47:06 +0430

    first version of express

---
+++ 14-express-typescript-integration/server/src/routes/loginRoutes.ts
@@ -0,0 +1,75 @@
+import {NextFunction, Request, Response, Router} from 'express';
+
+interface RequestWithBody extends Request {
+    body: {
+        [key: string]: string | undefined
+    }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (req.session && req.session.loggedIn) {
+        next();
+    } else {
+        res.status(403).send('Not permitted');
+    }
+}
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `);
+    } else {
+        res.send(`
+            <div>
+                <div>You are not logged in</div>
+                <a href="/login">Login</a>
+            </div>
+        `);
+    }
+});
+
+router.get('/login', (req: Request, res: Response) => {
+    res.send(`
+        <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="email" type="email">
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password">
+        </div>
+        <button>Submit</button>
+        </form>
+    `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const {email, password} = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+        // mark this person as logged in
+        req.session = {loggedIn: true};
+        // redirect them to the root route
+        res.redirect('/');
+    } else {
+        res.send('Invalid email or password');
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response, next: NextFunction) => {
+    res.send('Welcome to protected route, logged in user');
+});
+
+export {router};
